60c9a928f5483efd00c60f8d0b3bf080ba8cc619
pesche80 <peter.ambuehl@gmail.com>
2014-09-26 09:40:50 +0200

    Com

---
+++ ComTest_1_3/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ComTest_1_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetSerialPorts = new System.Windows.Forms.Button();
+            this.rtbIncomingData = new System.Windows.Forms.RichTextBox();
+            this.cboPorts = new System.Windows.Forms.ComboBox();
+            this.cboBaudRate = new System.Windows.Forms.ComboBox();
+            this.btnPortState = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGetSerialPorts
+            // 
+            this.btnGetSerialPorts.Location = new System.Drawing.Point(13, 13);
+            this.btnGetSerialPorts.Name = "btnGetSerialPorts";
+            this.btnGetSerialPorts.Size = new System.Drawing.Size(75, 23);
+            this.btnGetSerialPorts.TabIndex = 0;
+            this.btnGetSerialPorts.Text = "Ports";
+            this.btnGetSerialPorts.UseVisualStyleBackColor = true;
+            this.btnGetSerialPorts.Click += new System.EventHandler(this.btnGetSerialPorts_Click);
+            // 
+            // rtbIncomingData
+            // 
+            this.rtbIncomingData.Location = new System.Drawing.Point(13, 70);
+            this.rtbIncomingData.Name = "rtbIncomingData";
+            this.rtbIncomingData.Size = new System.Drawing.Size(259, 163);
+            this.rtbIncomingData.TabIndex = 1;
+            this.rtbIncomingData.Text = "";
+            // 
+            // cboPorts
+            // 
+            this.cboPorts.FormattingEnabled = true;
+            this.cboPorts.Location = new System.Drawing.Point(134, 13);
+            this.cboPorts.Name = "cboPorts";
+            this.cboPorts.Size = new System.Drawing.Size(121, 21);
+            this.cboPorts.TabIndex = 2;
+            this.cboPorts.SelectedIndexChanged += new System.EventHandler(this.cboPorts_SelectedIndexChanged);
+            // 
+            // cboBaudRate
+            // 
+            this.cboBaudRate.FormattingEnabled = true;
+            this.cboBaudRate.Location = new System.Drawing.Point(134, 41);
+            this.cboBaudRate.Name = "cboBaudRate";
+            this.cboBaudRate.Size = new System.Drawing.Size(121, 21);
+            this.cboBaudRate.TabIndex = 3;
+            this.cboBaudRate.SelectedIndexChanged += new System.EventHandler(this.cboBaudRate_SelectedIndexChanged);
+            // 
+            // btnPortState
+            // 
+            this.btnPortState.Location = new System.Drawing.Point(13, 41);
+            this.btnPortState.Name = "btnPortState";
+            this.btnPortState.Size = new System.Drawing.Size(75, 23);
+            this.btnPortState.TabIndex = 4;
+            this.btnPortState.Text = "closed";
+            this.btnPortState.UseVisualStyleBackColor = true;
+            this.btnPortState.Click += new System.EventHandler(this.btnPortState_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(289, 251);
+            this.Controls.Add(this.btnPortState);
+            this.Controls.Add(this.cboBaudRate);
+            this.Controls.Add(this.cboPorts);
+            this.Controls.Add(this.rtbIncomingData);
+            this.Controls.Add(this.btnGetSerialPorts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetSerialPorts;
+        private System.Windows.Forms.RichTextBox rtbIncomingData;
+        private System.Windows.Forms.ComboBox cboPorts;
+        private System.Windows.Forms.ComboBox cboBaudRate;
+        private System.Windows.Forms.Button btnPortState;
+    }
+}
+
---
+++ ComTest_1_3/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComTest_1_3
+{
+    public partial class Form1 : Form
+    {
+        SerialPort ComPort = new SerialPort();
+        public Form1()
+        {
+            //  Initialize Window
+            InitializeComponent();
+
+            //  Add baudrates to dropdown
+            cboBaudRate.Items.Add(300);
+            cboBaudRate.Items.Add(600);
+            cboBaudRate.Items.Add(1200);
+            cboBaudRate.Items.Add(2400);
+            cboBaudRate.Items.Add(9600);
+            cboBaudRate.Items.Add(14400);
+            cboBaudRate.Items.Add(19200);
+            cboBaudRate.Items.Add(38400);
+            cboBaudRate.Items.Add(57600);
+            cboBaudRate.Items.Add(115200);
+            cboBaudRate.Items.ToString();
+            cboBaudRate.Text = cboBaudRate.Items[0].ToString();
+        }
+
+        /// <summary>
+        ///  Reacts to a click on the button "Ports"
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnGetSerialPorts_Click(object sender, EventArgs e)
+        {
+                string[] ArrayComPortsNames = null;                     // Array for the names of connected COM Ports
+                int index = -1;                                         // index for COM Port array
+                string ComPortName = null;                              // string for chosen Port Name
+
+                btnPortState.Text = "closed";                           // write "closed" to button
+                ArrayComPortsNames = SerialPort.GetPortNames();         // look for installed COM Ports on computer and write in the array
+
+                do                                                      // write all installed COM Port names to the dropdown
+                {
+                    index += 1;
+                    cboPorts.Items.Add(ArrayComPortsNames[index]);
+                    // rtbIncomingData.Text += ArrayComPortsNames[index] + "\n";
+                }
+                while (!((ArrayComPortsNames[index] == ComPortName) || (index == ArrayComPortsNames.GetUpperBound(0))));
+
+                Array.Sort(ArrayComPortsNames);                         // sort COM Port array
+
+                if (index == ArrayComPortsNames.GetUpperBound(0))       // if only one COM Port installed
+                {
+                    ComPortName = ArrayComPortsNames[0];                // choose this COM Port
+                }
+                cboPorts.Text = ArrayComPortsNames[0];                  // write the first COM Port name visible to the dropdown
+
+               
+        }
+
+        private void cboPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboBaudRate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPortState_Click(object sender, EventArgs e)     // if Port State button is clicked
+        {
+            if (btnPortState.Text == "open")                            // and a COM Port is opened
+            {
+                ComPort.Close();                                        // close COM Port
+                btnPortState.Text = "closed";                           // and write closed on the button
+            }
+
+
+
+
+            else                                                        // if no COM Port is opened
+            {
+                try
+                {
+                    ComPort.PortName = Convert.ToString(cboPorts.Text); // try to set chosen COM Port
+                    ComPort.BaudRate = Convert.ToInt32(cboBaudRate.Text);   // and chosen baudrate
+                }
+                catch (InvalidOperationException ex)
+                {
+                    MessageBox.Show(ex.Message);                        // show error message if name or baudrate could not be set
+                }
+                
+
+                try
+                {
+                    ComPort.Open();                                     //  try to open the chosen COM Port
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    MessageBox.Show(ex.Message);                        // show error message if the port opening was unautorized
+                }
+                catch (InvalidOperationException ex)
+                {
+                    MessageBox.Show(ex.Message);                        // show error message if Port is already opened
+                }
+
+                if (ComPort.IsOpen)
+                {
+                    btnPortState.Text = "open";                         // write "open" to port state button, if port could be opened
+                }
+                else
+                {
+                    btnPortState.Text = "close";                        // write "closed" to port state button, if port could not be opened
+                    rtbIncomingData.Text = "Port \"" + Convert.ToString(ComPort.PortName) + "\" could not be opened"; // and write wich port could not be opened to the rich text box
+                }
+            }
+        }
+    }
+}
